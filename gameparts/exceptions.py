0444e4e97e472b51cfa715dd261e6542872cdf61
Саня Кубенин <shura.kubenin@mail.ru>
2024-12-12 00:45:19 +0300

    First commit

---
+++ gameparts/exceptions.py
@@ -0,0 +1,13 @@
+"""Новые исключения, которые могут возникнуть в ходе игры."""
+
+
+class FieldIndexError(IndexError):
+
+    def __str__(self):
+        return 'Введено значение за границами игрового поля'
+
+
+class CellOccupiedError(Exception):
+
+    def __str__(self):
+        return 'Попытка изменить занятую ячейку'
